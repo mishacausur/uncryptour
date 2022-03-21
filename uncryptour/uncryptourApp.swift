7d5d4943ec0ddeb7f669d46a843c64dbfdcbabf6
Misha Causur <mishacausur@mail.ru>
2022-03-22 00:38:58 +0500

    Initial Commit

---
+++ uncryptour/uncryptourApp.swift
@@ -0,0 +1,17 @@
+//
+//  uncryptourApp.swift
+//  uncryptour
+//
+//  Created by Misha Causur on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct uncryptourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
