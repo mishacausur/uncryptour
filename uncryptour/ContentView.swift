f579295e5eba6d243dfc96f41c358e31b31f21c0
Misha Causur <mishacausur@mail.ru>
2022-03-22 01:08:26 +0500

    task 1 in progress

--- uncryptour/ContentView.swift
+++ uncryptour/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                
+            }
         }
     }
 }
